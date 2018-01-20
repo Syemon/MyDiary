bacfd51dbb0959b788574d27a94d3956b7be1f7b
szymon linowski <sz.linowski@gmail.com>
2018-01-20 12:08:03 +0100

    Add pdf generator

--- src/AppBundle/Controller/DiaryController.php
+++ src/AppBundle/Controller/DiaryController.php
@@ -110,7 +110,7 @@ class DiaryController extends Controller
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $diary = $form->getData();
-            $diary->setUser($user);
+            //$diary->setUser($user);
             $file = $diary->getAttachment();
 
             // Upload only if attachment is not null and delete old file
@@ -119,7 +119,10 @@ class DiaryController extends Controller
                 $diary->setAttachment($fileName);
                 $path = $this->container->getParameter('file_directory');
                 $filesystem = new Filesystem();
-                $filesystem->remove("$path/$previousFile");
+                if ($previousFile != null)
+                {
+                    $filesystem->remove($path.'/'.$previousFile);
+                }
             }
 
             $em = $this->getDoctrine()->getManager();
@@ -172,5 +175,58 @@ class DiaryController extends Controller
     {
         $path = $this ->container->getParameter('file_directory');
         return new BinaryFileResponse("$path/$file");
+    }
+
+    /**
+     * @Route("/diary/{id}/pdf", name="diary_to_pdf")
+     * @Method("GET")
+     */
+    public function createDiaryPdfAction($id)
+    {
+        $path = $this->container->getParameter('pdf_directory');
+        $file = bin2hex(random_bytes(10)).'.pdf';
+
+        $em = $this->getDoctrine()->getRepository('AppBundle:Diary');
+        $diary = $em->findOneBy([
+            "id" => $id
+        ]);
+
+        $this->get('knp_snappy.pdf')->generateFromHtml(
+            $this->renderView('diary/pdf.html.twig', [
+                    'diary' => $diary,
+                    'base_dir' => $this->get('kernel')->getRootDir()
+                ]),
+            $path.'/'.$file
+        );
+
+        return new BinaryFileResponse($path.'/'.$file);
+    }
+
+    /**
+     * @Route("/diaries/{id}/pdf", name="diaries_to_pdf")
+     * @Method("GET")
+     */
+    public function createDiariesPdfAction($id)
+    {
+        $path = $this->container->getParameter('pdf_directory');
+        $file = bin2hex(random_bytes(10)).'.pdf';
+
+        $em = $this->getDoctrine()->getRepository('AppBundle:User');
+        $user = $em->findOneBy([
+            "id" => $id
+        ]);
+
+        $em = $this->getDoctrine()->getRepository('AppBundle:User');
+        $diary = $em->findAllUserDiaries($user);
+
+        $this->get('knp_snappy.pdf')->generateFromHtml(
+            $this->renderView('diary/many_pdf.html.twig', [
+                'diaries' => $diary,
+                'base_dir' => $this->get('kernel')->getRootDir()
+            ]),
+            $path.'/'.$file
+        );
+
+        return new BinaryFileResponse($path.'/'.$file);
     }
 }
