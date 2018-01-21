b860a9ab56f08f8f32d6461935e7905349e0f5ec
szymon linowski <sz.linowski@gmail.com>
2018-01-21 19:26:46 +0100

    Add link to the main page in the base.html.twig

--- src/AppBundle/Controller/MainController.php
+++ src/AppBundle/Controller/MainController.php
@@ -2,12 +2,14 @@
 
 namespace AppBundle\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class MainController extends Controller
 {
     /**
      * Redirects a visitor to the main page
+     * @Route("/", name="main_page")
      */
     public function homepageAction()
     {
