18541912d3e36a1b7f8ca60e2f9f9da610e0340d
szymon linowski <sz.linowski@gmail.com>
2018-12-09 14:10:15 +0100

    Fix depreciations

--- src/AppBundle/Security/LoginFormAuthenticator.php
+++ src/AppBundle/Security/LoginFormAuthenticator.php
@@ -32,13 +32,13 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
         $this->passwordEncoder = $passwordEncoder;
     }
 
+    public function supports(Request $request)
+    {
+        return $request->getPathInfo() == '/login' && $request->isMethod('POST');
+    }
+
     public function getCredentials(Request $request)
     {
-        $isLoginSubmit = $request->getPathInfo() == '/login' && $request->isMethod('POST');
-        if (!$isLoginSubmit) {
-            return;
-        }
-
         $form = $this->formFactory->create(LoginForm::class);
         $form->handleRequest($request);
 
